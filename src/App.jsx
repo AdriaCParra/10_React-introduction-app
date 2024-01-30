66a894115423f3bb5af6c1a69bdf4a2683168420
AdriaCParra <Costaadria524@gmail.com>
2024-01-30 18:39:53 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const name = "Adria Costa"
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <h2>{`My name is ${name}`}</h2>
+    </>
+  )
+}
+
+export default App
